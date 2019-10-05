6e1403581d4ce37cfd7c3274ad92df8b6d01108a
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-10-05 00:02:55 -0600

    Descifrado Espiral (Casi Completo)

--- Laboratorio_Cifrado/Utilities/Espiral.cs
+++ Laboratorio_Cifrado/Utilities/Espiral.cs
@@ -27,7 +27,7 @@ namespace Laboratorio_Cifrado.Utilities
             {
                 using (var reader = new BinaryReader(file))
                 {
-                    //Buffer para descifrar
+                    //Buffer para cifrar
                     while (reader.BaseStream.Position != reader.BaseStream.Length)
                     {
                         var buffer = reader.ReadBytes(count: bufferLength);
@@ -122,25 +122,9 @@ namespace Laboratorio_Cifrado.Utilities
             CifradoController.currentFile = rutaCifrado;
         }
 
-
         public static void Descifrar(string path, int password)
         {
-            using (var file = new FileStream(path, FileMode.Open))
-            {
-                using (var reader = new BinaryReader(file))
-                {
-                    //Buffer para descifrar
-                    while (reader.BaseStream.Position != reader.BaseStream.Length)
-                    {
-                        var buffer = reader.ReadBytes(count: bufferLength);
-                        foreach (var item in buffer)
-                        {
-
-                        }
-                    }
-                }
-            }
-
+            
             #region Crear_Archivo
 
             string NuevoArchivo = Path.GetFileName(path);
@@ -149,93 +133,116 @@ namespace Laboratorio_Cifrado.Utilities
 
             #endregion
 
-            #region Variables
-
-            FileInfo archivo = new FileInfo(path);
-            string texto = File.ReadAllText(path);
-
-            int m = password;
-            int n = (int) archivo.Length / m;
-            if (archivo.Length % m != 0) //Redondear al siguinete entero
+            using (var file = new FileStream(path, FileMode.Open))
             {
-                n++;
-            }
-
-            char[,] matriz = new char[n, m];
-            string respuesta = "";
-
-            int inicio = 0;
-            int limitefila = n;
-            int limitecolumna = m;
-
-            int valores = 1; //valores dentro de la matriz
-
-            int i = 0, j = 0;
-
-            #endregion
-            
-            //Escritura Espiral
-
-            #region Espiral
-
-            while (valores <= matriz.Length)
-            {
-
-                for (j = inicio; j < limitecolumna; j++)
+                using (var reader = new BinaryReader(file))
                 {
-                    matriz[i, j] = Convert.ToChar(texto.Substring(0, 1));
-                    texto = texto.Remove(0, 1);
-                    
-                    valores++;
-                }
-                for (i = inicio + 1; i < limitefila; i++)
-                {
-                    matriz[i, j-1] = Convert.ToChar(texto.Substring(0, 1));
-                    texto = texto.Remove(0, 1);
-
-
-                    valores++;
-                }
-                for (j = limitecolumna - 1; j > inicio && i > inicio + 1; j--)
-                {
-                    matriz[i-1, j-1] = Convert.ToChar(texto.Substring(0, 1));
-                    texto = texto.Remove(0, 1);
-
-                    valores++;
-                }
-                for (i = limitefila - 1; i > inicio + 1; i--)
-                {
-                    matriz[i-1, j] = Convert.ToChar(texto.Substring(0, 1));
-                    texto = texto.Remove(0, 1);
-
-                    valores++;
-                }
-
-                inicio++;
-                limitecolumna--;
-                limitefila--;
-            }
-            #endregion
-
-            //Lectura final
-
-            #region Matriz
-
-            for (int k = 0; k < m; k++)
-            {
-                for (int l = 0; l < n; l++)
-                {
-                    if( matriz[l, k] != EOF) 
+                    //Buffer para descifrar
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
                     {
-                        respuesta += matriz[l, k];
+                        
+                        #region Variables
+
+                        int bufferPosition = 0;
+
+                        int fileLength = bufferLength;
+
+                        int m = password;
+                        int n = (int)fileLength / m;
+                        if (fileLength % m != 0) { n++; }//Redondear al siguinete entero
+
+                        //Corregir error
+                        while (fileLength < (n*m))
+                        {
+                            fileLength++;
+                        }
+                        
+                        var buffer = reader.ReadBytes(count: fileLength);
+
+                        char[,] matriz = new char[n, m];
+                        List<byte> respuesta = new List<byte>();
+
+                        int inicio = 0;
+                        int limitefila = n;
+                        int limitecolumna = m;
+
+                        int valores = 1; //valores dentro de la matriz
+
+                        int i = 0, j = 0;
+
+                        FileStream fs = new FileStream(rutaDescifrado, FileMode.Append);
+                        BinaryWriter bw = new BinaryWriter(fs);
+
+                        #endregion
+
+                        //Escritura Espiral
+                        #region Espiral
+
+                        while (bufferPosition < buffer.Length)
+                        {
+                            for (j = inicio; j < limitecolumna; j++)
+                            {
+                                if (bufferPosition < buffer.Length)
+                                {
+                                    matriz[i, j] = (char) buffer[bufferPosition];
+                                    bufferPosition++;
+                                    valores++;
+                                }
+                            }
+                            for (i = inicio + 1; i < limitefila; i++)
+                            {
+                                if (bufferPosition < buffer.Length)
+                                {
+                                    matriz[i, j - 1] = (char) buffer[bufferPosition];
+                                    bufferPosition++;
+                                    valores++;
+                                }
+                            }
+                            for (j = limitecolumna - 1; j > inicio && i > inicio + 1; j--)
+                            {
+                                if (bufferPosition < buffer.Length)
+                                {
+                                    matriz[i - 1, j - 1] = (char) buffer[bufferPosition];
+                                    bufferPosition++;
+                                    valores++;
+                                }
+                            }
+                            for (i = limitefila - 1; i > inicio + 1; i--)
+                            {
+                                if (bufferPosition < buffer.Length)
+                                {
+                                    matriz[i - 1, j] = (char) buffer[bufferPosition];
+                                    bufferPosition++;
+                                    valores++;
+                                }
+                            }
+
+                            inicio++;
+                            limitecolumna--;
+                            limitefila--;
+                        }
+                        #endregion
+
+                        //Lectura final
+                        #region Matriz
+                        for (int k = 0; k < m; k++)
+                        {
+                            for (int l = 0; l < n; l++)
+                            {
+                                if (matriz[l, k] != EOF && matriz[l, k] != default(char))
+                                {
+                                    respuesta.Add((byte) matriz[l, k]);
+                                }
+                            }
+                        }
+                        #endregion
+                        
+                        bw.Write(respuesta.ToArray());
+                        bw.Close();
                     }
                 }
             }
-            
-            #endregion
 
-
-            File.WriteAllText(rutaDescifrado, respuesta);
             CifradoController.currentFile = rutaDescifrado;
         }
     }
